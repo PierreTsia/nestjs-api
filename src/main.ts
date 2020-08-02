9e1b034e43c365f589decbb7e7c957b851ac5b61
Pierre_T <pierre.tsiakkaros@gmail.com>
2020-08-02 15:57:04 +0200

    :rocket: prepare for heroku deploy

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new AllExceptionsFilter());
-  await app.listen(3000);
+  app.enableCors();
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
